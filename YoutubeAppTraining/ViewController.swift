2f1a29c37459fc0e4b553a56be159678146707a7
Marina <marina.mam.riad@gmail.com>
2020-08-13 12:47:16 -0700

    initial commit for project

---
+++ YoutubeAppTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeAppTraining
+//
+//  Created by Marina on 8/13/20.
+//  Copyright © 2020 Marina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
